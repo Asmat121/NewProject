2b3655ad686313d45ef28660777cbcac120d289f
Asmat <asmatnisar121@gmail.com>
2020-12-16 00:44:33 -0500

    new changes

---
+++ scriptt.js
@@ -0,0 +1,14 @@
+
+    <script>
+    function validate(){
+        var Username=document.getElementById("User").Value;
+        var passwoard=docoment.getElementById("Pass").value;
+        if(username=="Admin" && passwoard=="user"){
+            alert("login sucess");
+            return false;
+        }
+        else{
+            alert("login fail");
+        }
+    }
+</script>
